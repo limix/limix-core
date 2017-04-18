d5609999abdc6198196888305b67dd19fd7ea8d7
Danilo Horta <danilo.horta@gmail.com>
2017-04-18 15:05:27 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,57 @@
+from __future__ import unicode_literals
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except (OSError, IOError, ImportError):
+    long_description = open('README.md').read()
+
+
+def setup_package():
+    src_path = os.path.dirname(os.path.abspath(sys.argv[0]))
+    old_path = os.getcwd()
+    os.chdir(src_path)
+    sys.path.insert(0, src_path)
+
+    needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+    pytest_runner = ['pytest-runner>=2.9'] if needs_pytest else []
+
+    setup_requires = [] + pytest_runner
+    install_requires = []
+    tests_require = ['pytest']
+
+    metadata = dict(
+        name='limix-core',
+        version='1.0.0',
+        maintainer="Limix Developers",
+        maintainer_email="horta@ebi.ac.uk",
+        license="MIT",
+        description="Core for Limix.",
+        long_description=long_description,
+        url='https://github.com/limix/limix-core',
+        packages=find_packages(),
+        zip_safe=False,
+        install_requires=install_requires,
+        setup_requires=setup_requires,
+        tests_require=tests_require,
+        include_package_data=True,
+        classifiers=[
+            "Development Status :: 5 - Production/Stable",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ])
+
+    try:
+        setup(**metadata)
+    finally:
+        del sys.path[0]
+        os.chdir(old_path)
+
+
+if __name__ == '__main__':
+    setup_package()
